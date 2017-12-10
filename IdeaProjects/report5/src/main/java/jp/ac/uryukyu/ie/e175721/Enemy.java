36e91850b51a934ce2acf64884851b0337c987df
Takuma Motohara <e175721@ie.u-ryukyu.ac.jp>
2017-12-10 20:26:51 +0900

    second commit

--- IdeaProjects/report5/src/main/java/jp/ac/uryukyu/ie/e175721/Enemy.java
+++ IdeaProjects/report5/src/main/java/jp/ac/uryukyu/ie/e175721/Enemy.java
@@ -20,8 +20,9 @@ public class Enemy extends LivingThing {
             if (damage == 0) {
                 System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName());
             } else if (val == 0 || val == 1 || val == 2 ) {
+                damage = damage*2;
                 System.out.printf("%sの攻撃！痛恨の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
-                opponent.wounded(2 * damage);
+                opponent.wounded(damage);
             } else {
                 System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
             }
--- IdeaProjects/report5/src/main/java/jp/ac/uryukyu/ie/e175721/Hero.java
+++ IdeaProjects/report5/src/main/java/jp/ac/uryukyu/ie/e175721/Hero.java
@@ -19,8 +19,9 @@ public class Hero extends LivingThing {
             if (damage == 0) {
                 System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName());
             } else if (val == 0 || val == 1 || val == 2 || val == 3) {
+                damage = damage*2;
                 System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
-                opponent.wounded(2 * damage);
+                opponent.wounded(damage);
             } else {
                 System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
             }
@@ -37,7 +38,7 @@ public class Hero extends LivingThing {
         setHitPoint(getHitPoint() - damage);
         if( getHitPoint() < 0 ) {
             setDead(true);
-            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
+            System.out.printf("%sは道半ばで力尽きてしまった。\n", getName());
         }
     }
 }
